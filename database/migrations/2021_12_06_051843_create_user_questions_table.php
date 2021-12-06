a6cb5ef668b2d489cf82b9815c9196a7079b66da
Tata Redha Al Fath <tataredhaalfath13@gmail.com>
2021-12-06 12:22:15 +0700

    make migration create table user_questions dan user_answers

---
+++ database/migrations/2021_12_06_051843_create_user_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_questions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('pertanyaan_1');
+            $table->string('pertanyaan_2');
+            $table->string('pertanyaan_3');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_questions');
+    }
+}
---
+++ database/migrations/2021_12_06_052003_create_user_answers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_answers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('uq_id');
+            $table->integer('user_id');
+            $table->string('jawaban_1');
+            $table->string('jawaban_2');
+            $table->string('jawaban_3');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_answers');
+    }
+}
