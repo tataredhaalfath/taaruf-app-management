c9b221ceacc7c353231d6ab9b288703012ceba93
Tata Redha Al Fath <tataredhaalfath13@gmail.com>
2021-12-13 13:20:48 +0700

    memberbaiki pengambilan data untuk list taaruf approve

--- app/Http/Controllers/User/ApproveController.php
+++ app/Http/Controllers/User/ApproveController.php
@@ -12,40 +12,15 @@ class ApproveController extends Controller
 {
     public function index(Request $request)
     {
-        //MENERIMA
-        $approve1 = Taaruf::where('user_id_1', Auth::user()->id)
-            ->where('status', 'APPROVED')->first();
-        //DITERIMA
-        $approve2 = Taaruf::where('user_id_2', Auth::user()->id)
-            ->where('status', 'APPROVED')->first();
-
-        if ($approve1) {
-            $approve2 = null;
-            $user1 = User::findOrFail($approve1->user_id_1);
-            $user2 = User::findOrFail($approve1->user_id_2);
-            return view('pages.user.approve.index', [
-                'approve1' => $approve1,
-                'approve2' => $approve2,
-                'user1' => $user1,
-                'user2' => $user2,
-            ]);
-        } elseif ($approve2) {
-            $approve1 = null;
-            $user1 = User::findOrFail($approve2->user_id_1);
-            $user2 = User::findOrFail($approve2->user_id_2);
-            return view('pages.user.approve.index', [
-                'approve1' => $approve1,
-                'approve2' => $approve2,
-                'user1' => $user1,
-                'user2' => $user2,
-            ]);
-        } else {
-            $approve1 = null;
-            $approve2 = null;
-            return view('pages.user.approve.index', [
-                'approve1' => $approve1,
-                'approve2' => $approve2,
-            ]);
-        }
+        $approve = Taaruf::where('user_id_1', Auth::user()->id)
+            ->Where('status', 'APPROVED')
+            ->orWhere(function ($query) {
+                $query->where('user_id_2', Auth::user()->id)
+                    ->where('status', 'APPROVED');
+            })
+            ->get();
+        return view('pages.user.approve.index', [
+            'approve' => $approve
+        ]);
     }
 }
--- resources/views/pages/user/approve/index.blade.php
+++ resources/views/pages/user/approve/index.blade.php
@@ -1,3 +1,6 @@
+@php
+use App\Models\User;
+@endphp
 @extends('layouts.app')
 @section('title', "Ta'aruf App Management")
 
@@ -23,42 +26,34 @@
                             <div class="row my-3">
                                 <div class="col-lg-12 col-md-10">
                                     <table class="table table-bordered">
-                                        <tbody>
+                                        <tr class="text-center">
+                                            <th>No</th>
+                                            <th>User 1</th>
+                                            <th>User 2</th>
+                                            <th>Tanggal</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        @forelse ($approve as $app )
+                                            @php
+                                                $user1 = User::findOrFail($app->user_id_1);
+                                                $user2 = User::findOrFail($app->user_id_2);
+                                            @endphp
                                             <tr class="text-center">
-                                                <th>No</th>
-                                                <th>User 1</th>
-                                                <th>User 2</th>
-                                                <th>Tanggal</th>
-                                                <th>Status</th>
-                                                <th>Action</th>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $user1->name }}</td>
+                                                <td>{{ $user2->name }}</td>
+                                                <td>{{ date('d-m-Y', strtotime($app->created_at)) }}</td>
+                                                <td>{{ $app->status }}</td>
+                                                <td><a href="approved-confirm.html" class="btn-cta">Lanjut</a>
+                                                </td>
                                             </tr>
-                                            @if ($approve1)
-                                                <tr class="text-center">
-                                                    <td>1</td>
-                                                    <td>{{ $user1->name }}</td>
-                                                    <td>{{ $user2->name }}</td>
-                                                    <td>{{ date('d-m-Y', strtotime($approve1->created_at)) }}</td>
-                                                    <td>{{ $approve1->status }}</td>
-                                                    <td><a href="approved-confirm.html" class="btn-cta">Lanjut</a>
-                                                    </td>
-                                                </tr>
-                                            @elseif ($approve2)
-                                                <tr class="text-center">
-                                                    <td>1</td>
-                                                    <td>{{ $user1->name }}</td>
-                                                    <td>{{ $user2->name }}</td>
-                                                    <td>{{ date('d-m-Y', strtotime($approve2->created_at)) }}</td>
-                                                    <td>{{ $approve2->status }}</td>
-                                                    <td><a href="approved-confirm.html" class="btn-cta">Lanjut</a>
-                                                    </td>
-                                                </tr>
-                                            @else
-                                                <tr class="text-center">
-                                                    <td colspan="6">BELUM ADA PERMINTAAN TAARUF DITERIMA</td>
-                                                </tr>
-                                            @endif
+                                        @empty
+                                            <tr class="text-center">
+                                                <td colspan="6">BELUM ADA PERMINTAAN TAARUF DITERIMA</td>
+                                            </tr>
+                                        @endforelse
 
-                                        </tbody>
                                     </table>
                                 </div>
                             </div>
