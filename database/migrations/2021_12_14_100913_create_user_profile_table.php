f604b163f5c4e6400fb9e09df3afd070294e2613
Tata Redha Al Fath <tataredhaalfath13@gmail.com>
2021-12-14 17:15:06 +0700

    membuat table user_profile

---
+++ database/migrations/2021_12_14_100913_create_user_profile_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profile', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->text('image');
+            $table->string('telpon');
+            $table->string('gender');
+            $table->string('kota');
+            $table->string('kecamatan');
+            $table->string('kelurahan');
+            $table->string('alamat');
+            $table->integer('umur');
+            $table->string('pendidikan_ahir');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profile');
+    }
+}
